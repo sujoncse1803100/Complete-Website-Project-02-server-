6b8e615e0f39a35cc905b17065fe40f37b6de2aa
sujoncse1803100 <89827918+sujoncse1803100@users.noreply.github.com>
2021-11-26 01:17:10 +0600

    regular update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const fileUpload = require("express-fileupload");
 require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = 3001;
@@ -53,80 +54,100 @@ try {
       const newImage = file.data;
       const encodedImage = newImage.toString("base64");
 
-    const service = {
+      const service = {
         title: title,
         desc: description,
-        image: encodedImage
-    }
+        image: encodedImage,
+      };
 
-    serviceCollection.insertOne(service)
-        .then(result => {
-            if (result){
-                res.send(result.acknowledged);
-                console.log("Service Inserted");
-            }
-        })
-
-    });
-
-    app.get('/service',(req, res) => {
-        serviceCollection.find({})
-          .toArray((err,result)=>{
-              res.send(result);
-          })
-    });
-
-    app.post('/makeAdmin',(req, res) => {
-        const email = req.body.email;
-        console.log(email);
-
-        adminCollection.insertOne({email: email})
-            .then(result => {
-                res.send(result.acknowledged);
-            });
-    });
-
-    app.post('/isAdmin',(req, res) => {
-        const email = req.body.email;
-        adminCollection.find({email: email})
-          .toArray((err,result)=>{
-              res.send(result.length > 0);
-          });
-    });
-
-
-    app.post('/createOrder',(req, res) => {
-        const data = req.body;
-        const newImage = req.files.file.data;
-        const encodedImage = newImage.toString('base64');
-
-        const order = {
-            name : data.name,
-            email : data.email,
-            orderName : data.orderName,
-            projectDetails : data.projectDetails,
-            price : data.price,
-            image : encodedImage
+      serviceCollection.insertOne(service).then((result) => {
+        if (result) {
+          res.send(result.acknowledged);
+          console.log("Service Inserted");
         }
+      });
+    });
 
-        orderCollection.insertOne(order)
-        .then(result => {
-            if (result){
-                res.send(result.acknowledged);
-                console.log("Order Placed");
+    app.get("/service", (req, res) => {
+      serviceCollection.find({}).toArray((err, result) => {
+        res.send(result);
+      });
+    });
+
+    app.post("/makeAdmin", (req, res) => {
+      const email = req.body.email;
+      console.log(email);
+
+      adminCollection.insertOne({ email: email }).then((result) => {
+        res.send(result.acknowledged);
+      });
+    });
+
+    app.post("/isAdmin", (req, res) => {
+      const email = req.body.email;
+      adminCollection.find({ email: email }).toArray((err, result) => {
+        res.send(result.length > 0);
+      });
+    });
+
+    app.post("/createOrder", (req, res) => {
+      const data = req.body;
+      const newImage = req.files.file.data;
+      const encodedImage = newImage.toString("base64");
+
+      const status = {
+        selectedStatus: data.selectedStatus,
+        secondStatus: data.secondStatus,
+        thirdStatus: data.thirdStatus,
+      };
+
+      const order = {
+        name: data.name,
+        email: data.email,
+        orderName: data.orderName,
+        projectDetails: data.projectDetails,
+        price: data.price,
+        status: status,
+        statusColor: data.statusColor,
+        image: encodedImage,
+      };
+
+      // console.log(status);
+
+      orderCollection.insertOne(order).then((result) => {
+        if (result) {
+          res.send(result.acknowledged);
+          console.log("Order Placed");
+        }
+      });
+    });
+
+    app.get("/orders", (req, res) => {
+      orderCollection.find({}).toArray((err, documents) => {
+        res.send(documents);
+      });
+    });
+
+    app.patch("/updateStatus/:_id", (req, res) => {
+      const status = {
+        selectedStatus: req.body.selectedStatus,
+        secondStatus: req.body.secondStatus,
+        thirdStatus: req.body.thirdStatus,
+      };
+      const statusColor = req.body.dropdownColor;
+
+      if (status && statusColor) {
+        orderCollection.updateOne({ _id: ObjectId(req.params._id) },{
+              $set: {
+                status: status,
+                statusColor: statusColor
+              },
             }
-        });
-
-        
-        // console.log(order);
-    })
-
-
-    app.get('/orders',(req, res) => {
-        orderCollection.find({})
-        .toArray((err,documents) => {
-            res.send(documents);
-        })
+          )
+          .then((result) => {
+            res.send(result.modifiedCount > 0);
+          });
+      }
     });
 
 
